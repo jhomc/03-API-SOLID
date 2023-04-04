ce9c7e69178a2f16415f3998dea39a97a1ad3129
Jhonata Costa <jhomcosta@gmail.com>
2023-04-04 13:51:52 -0300

    get-user-profile use case e testes

--- src/repositories/in-memory/in-memory-users-repoitory.ts
+++ src/repositories/in-memory/in-memory-users-repoitory.ts
@@ -4,6 +4,16 @@ import { UsersRepository } from '../users-respository'
 export class InMemoryUsersRepository implements UsersRepository {
   public items: User[] = []
 
+  async findById(id: string): Promise<User | null> {
+    const user = this.items.find((user) => user.id === id)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
   async create(data: Prisma.UserCreateInput) {
     const user = {
       id: 'user-1',
