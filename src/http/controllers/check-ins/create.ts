9fd817c7ae214d2b6408c978f2209f9fc25b0ee2
Jhonata Costa <jhomcosta@gmail.com>
2023-05-15 10:31:03 -0300

    demais controllers

---
+++ src/http/controllers/check-ins/create.ts
@@ -0,0 +1,30 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { makeCheckInsUseCase } from '@/use-cases/factories/make-check-in-use-case'
+
+export async function create(request: FastifyRequest, reply: FastifyReply) {
+  const createCheckInQuerySchema = z.object({
+    gymId: z.string().uuid(),
+  })
+  const createCheckInBodySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+  })
+
+  const { gymId } = createCheckInQuerySchema.parse(request.params)
+  const { latitude, longitude } = createCheckInBodySchema.parse(request.body)
+
+  const checkInUseCase = makeCheckInsUseCase()
+  await checkInUseCase.execute({
+    userId: request.user.sub,
+    gymId,
+    userLatitude: latitude,
+    userLongitude: longitude,
+  })
+
+  return reply.status(201).send()
+}
